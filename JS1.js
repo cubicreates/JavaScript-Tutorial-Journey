93660f15951fee0dcedddabd6b08b77a0586300f
Adit Ghosh <blubrdpirts@gmail.com>
2024-09-29 21:18:03 +0530

    Datatypes and Typecasting

--- JS1.js
+++ JS1.js
@@ -4,3 +4,25 @@ console.log("Hello World")
 // User Input
 variable =prompt("What is your name?: ")
 console.log(variable)
+
+//Datatype and Typecasting
+myNum = 5;               // Integer (whole number)
+myFloatNum = 5.99;    // Floating point number
+myLetter = 'D';         // Character
+myBool = true;       // Boolean
+myText = "Hello";     // String
+
+console.log(myNum, myFloatNum, myLetter, myBool, myText)
+
+a = '7'
+console.log(a, typeof a)
+
+b = parseInt(a)
+console.log(b, typeof b)
+
+c = parseFloat(a)
+console.log(c, typeof c)
+
+d = 7
+e = String(d)
+console.log(e, typeof e)
